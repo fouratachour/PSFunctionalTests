5d59449f2d805d27ab6e8aed8619024a1ff7b42e
Fourat ACHOUR <fourat.achour@sifast.tn>
2017-05-03 09:59:14 +0100

    testing maildev

--- test/itg/1.7/index.webdriverio.js
+++ test/itg/1.7/index.webdriverio.js
@@ -34,7 +34,8 @@ describe('Allscenario', function(){
 	//create an order in FO and check it in BO
 	//require('./scenario/FO/buy_product.webdriverio');
 	//require('./scenario/FO/check_module_notification.webdriverio');
-	require('./scenario/BO/invoiceterms.webdriverio');
+	//require('./scenario/BO/invoiceterms.webdriverio');
+	require('./scenario/BO/email_module.webdriverio');
 	//require('./scenario/BO/check_order.webdriverio');
 
 	//create an account in FO
---
+++ test/itg/1.7/scenario/BO/email_module.webdriverio.js
@@ -0,0 +1,67 @@
+'use strict';
+var should = require('should');
+var common = require('../../common.webdriverio');
+var globals = require('../../globals.webdriverio.js');
+
+describe('The Check of the order in Back Office', function(){
+	common.initMocha.call(this);
+	
+	before(function(done){
+		this.selector = globals.selector;
+		this.client.call(done);
+	});
+	process.on('uncaughtException', common.take_screenshot);
+	process.on('ReferenceError', common.take_screenshot);
+	after(common.after);
+	
+	describe('Log in in Back Office', function(done){
+        it('should log in successfully in BO', function(done){
+            global.fctname= this.test.title;
+		    this.client
+                .signinBO()
+                .waitForExist(this.selector.menu, 90000)
+                .call(done);
+		});
+	});
+	
+	describe('Send email test', function(done){
+
+        it('should go to Advanced Parameters', function(done){
+            global.fctname= this.test.title;
+		    this.client
+		        .moveToObject('//*[@id="subtab-AdminAdvancedParameters"]/a')
+                .click('//*[@id="subtab-AdminEmails"]/a')
+                .pause(2000)
+                .click('//*[@id="mail_fieldset_test"]/div[4]/button')
+                .call(done);
+	    });
+
+	});
+
+    describe('verify Send email test', function(done){
+
+        it('should go to mailDev Parameters', function(done){
+            global.fctname= this.test.title;
+		    this.client
+                .url('http://localhost:1080')
+                .click('/html/body/div/div[1]/div[2]/ul/li[1]')
+                .pause(2000)
+                .getText('/html/body/div/div[2]/div[2]/div/div[1]').then(function(text) {
+					var my_text_check = text;
+					should(my_text_check).be.equal('This is a test message. Your server is now configured to send email.');
+				})
+
+                .call(done);
+	    });
+
+	});
+	
+	describe('Log out in Back Office', function(done){
+        it('should log out successfully in BO', function(done){
+            global.fctname= this.test.title;
+		    this.client
+                .signoutBO()
+                .call(done);
+		});
+	});
+});
