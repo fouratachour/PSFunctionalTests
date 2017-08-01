7e93cdddf88df9c27c4576749fe3ccc11745cc57
Fourat ACHOUR <fourat.achour@sifast.tn>
2017-08-01 12:08:11 +0100

    adding  working on upload travis

--- bin/upload.sh
+++ bin/upload.sh
@@ -3,7 +3,7 @@
 FIXED_BRANCH=$(echo $TRAVIS_BRANCH | sed 's/\//-/g')
 ARCHIVE=$FIXED_BRANCH-$(date +%Y-%m-%d_%H_%M_%S)-$TRAVIS_COMMIT.zip
 echo "Creating archive $ARCHIVE"
-tar -cjf $ARCHIVE -C test/itg/$PS_VERSION/mochawesome-report .
+tar -cjf $ARCHIVE -C test/itg/$PS_VERSION/mochawesome-report/* .
 FILESIZE=$(stat -c%s "$ARCHIVE")
 echo "Finished archive (size $FILESIZE), starting Google Drive upload"
 ./bin/gdrive-linux-x64 upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
