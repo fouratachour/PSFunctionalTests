8fb8cdff6b8713b3e938d8fcaa549f91699b7070
Fourat ACHOUR <fourat.achour@sifast.tn>
2017-08-01 15:13:11 +0100

    testing the creation of the file

--- bin/upload.sh
+++ bin/upload.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
 FIXED_BRANCH=$(echo $TRAVIS_BRANCH | sed 's/\//-/g')
-ARCHIVE=$FIXED_BRANCH-$(date +%Y-%m-%d_%H_%M_%S)-$TRAVIS_COMMIT.tar
+ARCHIVE=$FIXED_BRANCH-$(date +%Y-%m-%d_%H_%M_%S)-$TRAVIS_COMMIT
 echo "Creating archive $ARCHIVE"
-cd test/itg/$PS_VERSION/mochawesome-report/
-cd ../../..
-tar -cvf $ARCHIVE -C test/itg/$PS_VERSION/mochawesome-report/
+tar -cvf $ARCHIVE -C test/itg/$PS_VERSION/mochawesome-report .
 FILESIZE=$(stat -c%s "$ARCHIVE")
 echo "Finished archive (size $FILESIZE), starting Google Drive upload"
 ./bin/gdrive-linux-x64 upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
