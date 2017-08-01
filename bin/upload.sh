8433b63fa6c329886ad7a9833a30905d74a1ec36
Fourat ACHOUR <fourat.achour@sifast.tn>
2017-08-01 17:08:37 +0100

    add extention to the file name

--- bin/upload.sh
+++ bin/upload.sh
@@ -4,7 +4,7 @@ FIXED_BRANCH=$(echo $TRAVIS_BRANCH | sed 's/\//-/g')
 
 echo "Creating archive $ARCHIVE"
 
-tar -cjf reporter -C test/itg/$PS_VERSION/mochawesome-report .
+tar -cjf reporter.zip -C test/itg/$PS_VERSION/mochawesome-report .
 
 ./bin/gdrive-linux-x64 upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "reporter"
 
