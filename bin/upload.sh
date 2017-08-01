f76edda6804cf8f52f1a7708c1f5b389662e0617
Fourat ACHOUR <fourat.achour@sifast.tn>
2017-08-01 17:43:38 +0100

    Adding specific name to the uploaded file on Gdrive

--- bin/upload.sh
+++ bin/upload.sh
@@ -4,9 +4,13 @@ FIXED_BRANCH=$(echo $TRAVIS_BRANCH | sed 's/\//-/g')
 
 echo "Creating archive $ARCHIVE"
 
-zip -r zipfile.zip test/itg/$PS_VERSION/mochawesome-report
+ARCHIVE=$FIXED_BRANCH-$(date +%Y-%m-%d_%H_%M_%S)-$TRAVIS_COMMIT.zip
+
+echo "Creating archive $ARCHIVE"
+
+zip -r $ARCHIVE test/itg/$PS_VERSION/mochawesome-report/*
 
 
-./bin/gdrive-linux-x64 upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "zipfile.zip"
+./bin/gdrive-linux-x64 upload --refresh-token $GDRIVE_REFRESH_TOKEN --parent $GDRIVE_DIR "$ARCHIVE"
 
 echo "Finished Google Drive upload"
